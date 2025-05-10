854910308420714c3b14c5951dc853de46bdc99a
alvarop0777 <personalalvaropineros0777@gmail.com>
2025-05-09 19:12:08 -0500

    primeros dos retos

---
+++ reto1_video.py
@@ -0,0 +1,4 @@
+for i in range(0,10):
+    print(" ")
+    for i in range(i,10):
+        print(i,end="")
---
+++ reto2_video.py
@@ -0,0 +1,2 @@
+#reto de calculara la media de puntuacion con un lista
+#no lo hice pues pq ya se ahcerlo con una funcion suma y division 
